0b224dc767511be903f001ad1b5a9b38f08506e3
gonzalorportfolio <gromeroeducation@gmail.com>
2023-11-28 13:52:45 -0500

    initial commit

---
+++ app/src/main.js
@@ -0,0 +1,4 @@
+//can be main.js if you update change it in the html
+import './style.css'
+
+console.log('hello');
---
+++ app/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // GitHub Pages expects an index.html in the root directory
+  // so just run npm build before pushing to GitHub and this will rebuild our assets to the root
+  build: { outDir: '..' },
+  base: '/my-example/', // needed for github pages just put the repo name here
+});
